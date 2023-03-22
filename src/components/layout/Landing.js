541407b7cdd2b3ef9be2a2aca1f4ed263cb88821
Nevres <nevres_muratovic@hotmail.com>
2023-03-22 12:24:15 +0100

    initial commit

---
+++ src/components/layout/Landing.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Landing = () => {
+  return (
+    <div className='landing'>
+      <div className="overlay">
+        <div className="landing-inner">
+          <h1>Prljavi Jahači</h1>
+        </div>
+        <div>
+          <a href="/#">Login</a>
+          <a href="/#">Register</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Landing
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <ul>
+        <li>Register</li>
+        <li>Login</li>
+        <li>Bikes</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
