7f24dd4b107a95f52160025ef222f4e00b1c6dc2
Nevres <nevres_muratovic@hotmail.com>
2023-05-27 15:35:27 +0200

    modified config

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_YT_FB_API_KEY,
+  authDomain: process.env.REACT_APP_YT_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_YT_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_YT_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_YT_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_YT_APP_ID
+};
+
+export default firebaseConfig;
